0dc304d8c44b1764ae2d51a44d0a761fb38a2f7a
professormichael <77131080+professormichael@users.noreply.github.com>
2023-10-02 13:50:22 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+// js.js
+
+// Define a variable in the external JavaScript file
+var copy1 = "This is copy1 from external JavaScript!";
+function external1(){
+    alert('The Ready Status')
+}
+var phonewidth = 800;
+var tabletwidth = 900;
+var laptopwidth = 1340;
